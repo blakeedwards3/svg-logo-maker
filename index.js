1095d00d557a714e365c3476f337b93806fce02f
blakeedwards3 <blakeedwards.nc@gmail.com>
2023-04-11 18:17:46 -0400

    Added questions for the logo to index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Circle, Square } = require('./lib/shapes');
+
+// Questions for logo details
+const questions = [{
+    type: 'input',
+    name: 'text',
+    message: 'Enter up to 3 characters for your logo:',
+    validate: async (input) => {
+        if (input.length > 3) {
+            return "Error: Too many characters.";
+        } else {
+            return true;
+        }
+    }
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter a color for your text:',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Select a shape for your logo:',
+        choices: ['Triangle', 'Circle', 'Square'],
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter a color for your shape:'
+    }
+];
